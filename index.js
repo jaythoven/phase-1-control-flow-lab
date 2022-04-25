710e9e598b7a7baeb5e21df1c0c15c7772881b42
Jonathan Aguilar <j.aguilar0390@gmail.com>
2022-04-24 23:51:19 -0400

    commit tree 1205f55d1c9530e436be16f4645056814a9aeaa2

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(integer){
+  if (integer <= 400) {
+    return 'This one is on me!'
+  } else if (integer > 400 && integer < 2000) {
+    return 'That will be twenty bucks.'
+  } else if (integer > 2000 && integer < 2500) {
+    return 'I will gladly take your thirty bucks.'
+  } else {
+    return 'No can do.'
+  }
+ } 
+
+function ternaryCheckCity(cityName){
+  return cityName === 'NYC' ? 'Ok, sounds good.' : 'No go.'
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
-
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(money){
+  switch(money) {
+    case 'generous':
+      return 'Thank you so much.';
+    case 'not as generous':
+      return 'Thank you.';
+    default:
+      return 'Bye.'
+  }
 }
